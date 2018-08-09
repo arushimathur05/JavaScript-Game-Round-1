e3892d6d6ab0936fbae9fea1d1c60bc371551e23
David Larsen <david.larsen@gmail.com>
2018-08-08 18:35:10 -0700

    latest and greatest

--- js/input.js
+++ js/input.js
@@ -10,4 +10,14 @@ function keyPressed() {
     } else if (keyCode === UP_ARROW) {
         game.player.r += -1;
     }
+
+    // when player goes off the screen left
+    if (game.player.c < 0) game.player.c = COLS - 1;
+    // when player goes off the screen top
+    if (game.player.r < 0) game.player.r = ROWS - 1;
+    
+    // when player goes off the screen bottom
+    if (game.player.r == ROWS) game.player.r = 0;
+    
+    game.board[game.player.r][game.player.c] = P1;
 }
